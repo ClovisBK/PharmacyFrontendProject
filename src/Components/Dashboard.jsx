172d253a61bfb25007f84ca083b9242274857fd1
Kebeh Clovis Bin <kebehclovisbin@gmail.com>
2026-01-16 14:56:53 +0100

    Added dynamic colors for the background and text of the orders

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Styles/dashboard.css'
 import { format } from 'date-fns'
 import api from '../api'
+import { style } from 'framer-motion/client'
 const Dashboard = () => {
   const [orders, setOrders] = useState([]);
   const [error, setError] = useState('');
@@ -18,7 +19,28 @@ const Dashboard = () => {
     }
     LoadUserOrders();
   }, [])
-    
+    //creating the colors for the status
+    const getStatusColor = (status) => {
+      const statusMap = {
+        pending: {
+          bg: 'rgba(182, 119, 3, 0.14)',
+          text: '#f39c12'
+        },
+     
+        delivered: {
+          bg: 'rgba(14, 90, 4, 0.14)',
+          text: '#27ae60'
+
+        },
+        deleted: {
+          bg: 'rgba(245, 5, 5, 0.14)',
+          text: '#e74c3c'
+          
+        }
+      };
+      const normalizedStatus = status?.toLowerCase();
+      return statusMap[normalizedStatus] || {bg: 'rgba(149,165,166,0.14)', text: 'green'}
+    }
   return (
     <div className='dashboard-container'>
        <div className="dashboard-heading">
@@ -33,18 +55,26 @@ const Dashboard = () => {
        </div>
        <h1 className='order-section'>Order History</h1>
        <div className="order-history">
-        {orders.map(order => (
+        {orders.map(order => {
+          const styles = getStatusColor(order.status);
+          return (
           <div key={order.id} className='order-card'>
             <div className='right'>
               <div className='order-text'>{order.pharmacyName}</div>
               <div className="date-status">
                   <div className='date'>{format(new Date(order.orderDate), "MMM, dd yyyy")}</div>
-                  <div className='status'>{order.status}</div>
+                  <div className='status'
+                    style={{
+                      backgroundColor: styles.bg,
+                      color: styles.text
+                    }}
+                  >{order.status}</div>
               </div>
             </div>
             <div className='total-price'>{order.totalPrice} FCFA</div>
           </div>
-        ))}
+        );
+          })}
        </div>
     </div>
   )
